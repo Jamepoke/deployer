6e70a77b4cf82c934951e836703eba7f436893d2
Jameson Brown <jamesonbrown76@gmail.com>
2023-03-09 13:59:28 -0600

    initial commit

---
+++ netlify/functions/deploy.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const { schedule } = require('@netlify/functions')
+
+const handler = async (event, context) => {
+    const res = await axios(`https://api.render.com/v1/services/${process.env.RENDER_SERVICE_ID}/deploys`, {
+        method: 'post',
+        headers: {
+            'Accept': ' application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + process.env.RENDER_SERVICE_TOKEN
+        },
+        data: {}
+    })
+    const src = res.data
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(src)
+    }
+
+}
+
+exports.handler = schedule("@daily", handler)
